03b14ab82e3c3a86b2c09255febc17df604955b9
Metoděj Janota <metodejjanota@gmail.com>
2025-04-05 00:16:48 +0200

    Neo tree update

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -7,6 +7,17 @@ return {
         "MunifTanjim/nui.nvim",
     },
     config = function()
+        require("neo-tree").setup({
+            filesystem = {
+                filtered_items = {
+                    visible = true,
+                    show_hidden_count = true,
+                    hide_dotfiles = false,
+                    hide_gitignored = true,
+                    never_show = {},
+                },
+            },
+        })
         vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
     end
 }
